a3a3c85c2f314a1052a43d2e3092ca7b25c7ef75
timcastillogill <timgillviola@gmail.com>
2023-08-10 11:26:40 +0100

    Project setup and recievable entity and table setup

--- src/main/java/com/example/demo/Receivable.java
+++ src/main/java/com/example/demo/Receivable.java
@@ -1,2 +1,233 @@
-package com.example.demo;public class Receivable {
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Receivable")
+public class Receivable {
+	@Id
+	private Long id;
+	private String reference;
+	private String currencyCode;
+	private String issueDate;
+	private double openingValue;
+	private double paidValue;
+	private String dueDate;
+	private String closedDate;
+	private boolean cancelled;
+	private String debtorName;
+	private String debtorReference;
+	private String debtorAddress1;
+	private String debtorAddress2;
+	private String debtorTown;
+	private String debtorState;
+	private String debtorZip;
+	private String debtorCountryCode;
+	private String debtorRegistrationNumber;
+
+	public Receivable(Long id,
+					  String reference,
+					  String currencyCode,
+					  String issueDate,
+					  double openingValue,
+					  double paidValue,
+					  String dueDate,
+					  String closedDate,
+					  boolean cancelled,
+					  String debtorName,
+					  String debtorReference,
+					  String debtorAddress1,
+					  String debtorAddress2,
+					  String debtorTown,
+					  String debtorState,
+					  String debtorZip,
+					  String debtorCountryCode,
+					  String debtorRegistrationNumber) {
+		this.id = id;
+		this.reference = reference;
+		this.currencyCode = currencyCode;
+		this.issueDate = issueDate;
+		this.openingValue = openingValue;
+		this.paidValue = paidValue;
+		this.dueDate = dueDate;
+		this.closedDate = closedDate;
+		this.cancelled = cancelled;
+		this.debtorName = debtorName;
+		this.debtorReference = debtorReference;
+		this.debtorAddress1 = debtorAddress1;
+		this.debtorAddress2 = debtorAddress2;
+		this.debtorTown = debtorTown;
+		this.debtorState = debtorState;
+		this.debtorZip = debtorZip;
+		this.debtorCountryCode = debtorCountryCode;
+		this.debtorRegistrationNumber = debtorRegistrationNumber;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getCurrencyCode() {
+		return currencyCode;
+	}
+
+	public void setCurrencyCode(String currencyCode) {
+		this.currencyCode = currencyCode;
+	}
+
+	public String getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(String issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public double getOpeningValue() {
+		return openingValue;
+	}
+
+	public void setOpeningValue(double openingValue) {
+		this.openingValue = openingValue;
+	}
+
+	public double getPaidValue() {
+		return paidValue;
+	}
+
+	public void setPaidValue(double paidValue) {
+		this.paidValue = paidValue;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getClosedDate() {
+		return closedDate;
+	}
+
+	public void setClosedDate(String closedDate) {
+		this.closedDate = closedDate;
+	}
+
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+	public String getDebtorName() {
+		return debtorName;
+	}
+
+	public void setDebtorName(String debtorName) {
+		this.debtorName = debtorName;
+	}
+
+	public String getDebtorReference() {
+		return debtorReference;
+	}
+
+	public void setDebtorReference(String debtorReference) {
+		this.debtorReference = debtorReference;
+	}
+
+	public String getDebtorAddress1() {
+		return debtorAddress1;
+	}
+
+	public void setDebtorAddress1(String debtorAddress1) {
+		this.debtorAddress1 = debtorAddress1;
+	}
+
+	public String getDebtorAddress2() {
+		return debtorAddress2;
+	}
+
+	public void setDebtorAddress2(String debtorAddress2) {
+		this.debtorAddress2 = debtorAddress2;
+	}
+
+	public String getDebtorTown() {
+		return debtorTown;
+	}
+
+	public void setDebtorTown(String debtorTown) {
+		this.debtorTown = debtorTown;
+	}
+
+	public String getDebtorState() {
+		return debtorState;
+	}
+
+	public void setDebtorState(String debtorState) {
+		this.debtorState = debtorState;
+	}
+
+	public String getDebtorZip() {
+		return debtorZip;
+	}
+
+	public void setDebtorZip(String debtorZip) {
+		this.debtorZip = debtorZip;
+	}
+
+	public String getDebtorCountryCode() {
+		return debtorCountryCode;
+	}
+
+	public void setDebtorCountryCode(String debtorCountryCode) {
+		this.debtorCountryCode = debtorCountryCode;
+	}
+
+	public String getDebtorRegistrationNumber() {
+		return debtorRegistrationNumber;
+	}
+
+	public void setDebtorRegistrationNumber(String debtorRegistrationNumber) {
+		this.debtorRegistrationNumber = debtorRegistrationNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Receivable{" +
+				"id=" + id +
+				", reference='" + reference + '\'' +
+				", currencyCode='" + currencyCode + '\'' +
+				", issueDate='" + issueDate + '\'' +
+				", openingValue=" + openingValue +
+				", paidValue=" + paidValue +
+				", dueDate='" + dueDate + '\'' +
+				", closedDate='" + closedDate + '\'' +
+				", cancelled=" + cancelled +
+				", debtorName='" + debtorName + '\'' +
+				", debtorReference='" + debtorReference + '\'' +
+				", debtorAddress1='" + debtorAddress1 + '\'' +
+				", debtorAddress2='" + debtorAddress2 + '\'' +
+				", debtorTown='" + debtorTown + '\'' +
+				", debtorState='" + debtorState + '\'' +
+				", debtorZip='" + debtorZip + '\'' +
+				", debtorCountryCode='" + debtorCountryCode + '\'' +
+				", debtorRegistrationNumber='" + debtorRegistrationNumber + '\'' +
+				'}';
+	}
 }
